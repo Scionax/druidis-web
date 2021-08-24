6af4ac664438a53a801f056f94751bf4e4f8a5b1
Scionax <ScionaxNG@gmail.com>
2021-08-23 21:14:57 -0500

    Improving OpenGraph Script

--- public/scripts/open-graph-script.js
+++ public/scripts/open-graph-script.js
@@ -7,7 +7,14 @@
 		4. Press the bookmark when visiting a website that you want content from.
 */
 
-const DruidisHost = "http://api.dev.druidis";
+const zqqConfig = {
+	host: "http://api.dev.druidis",
+	forum: "Technology",
+	categories: ["News", "Events", "Other"],
+	defaultCategory: "Events",
+	autoSubmit: false,
+	allowComments: false,
+};
 
 // metaData: Data Structure for an OpenGraph Page
 class ogMetaData {
@@ -15,10 +22,13 @@ class ogMetaData {
 	constructor() {
 		this.url = "";
 		this.title = "";
-		this.image = new metaVisual();
-		this.video = new metaVisual();
+		this.image = new ogMetaVisual();
+		this.video = new ogMetaVisual();
 		// this.audio = new metaAudio;
-		this.description = "";
+		this.content = "";
+		this.comment = "";
+		
+		// Miscellaneous
 		this.determiner = "";			// Word that appears before object's title in a sentence. Enum of (a, an, the, "", auto).
 		this.locale = "";
 		this.site_name = "";
@@ -52,8 +62,8 @@ class ogMetaData {
 	}
 }
 
-// metaVisual: Data Structure for Images and Videos
-class metaVisual {
+// ogMetaVisual: Data Structure for Images and Videos
+class ogMetaVisual {
 	
 	constructor() {
 		this.url = "";
@@ -79,29 +89,30 @@ class metaVisual {
 	}
 }
 
-function parseMetaTags(metaData) {
+function zqqParseMetaTags(metaData) {
 	
 	// Trackers
-	var scanForType = "";	// As we loop through meta tags, some are based on the last ones located (such as 'image' and 'video'), so track the 'current' set.
+	let scanForType = "";	// As we loop through meta tags, some are based on the last ones located (such as 'image' and 'video'), so track the 'current' set.
+	const metaElements = document.getElementsByTagName("meta");
 	
 	// Loop through every meta value.
-	for(var b = document.getElementsByTagName("meta"), a = 0; a < b.length; a++) {
+	for(a = 0; a < metaElements.length; a++) {
 		
 		// Get the MetaTag Data
-		var metaVals = b[a];
-		var metaName = metaVals.getAttribute("property") ? metaVals.getAttribute("property") : metaVals.name;
+		const metaVals = metaElements[a];
+		const metaName = metaVals.getAttribute("property") ? metaVals.getAttribute("property") : metaVals.name;
 		if(!metaName || !metaVals.content) { continue; }
-		var metaContent = metaVals.content;
+		const metaContent = metaVals.content;
 		
 		// Split to detect sets:
-		var s = metaName.split(":");		// Splits into 2 or 3 parts. Ex: "og:image:width" -> ['og', 'image', 'width']
-		var name = s[1];
-		var nameProp = s[2];
+		const s = metaName.split(":");		// Splits into 2 or 3 parts. Ex: "og:image:width" -> ['og', 'image', 'width']
+		const name = s[1];
+		const nameProp = s[2];
 		
 		// Apply Detected Content to Values
 		if(name == "url") { metaData.url = metaContent; scanForType = ""; }
 		if(name == "title") { metaData.title = metaContent; scanForType = ""; }
-		if(name == "description") { metaData.description = metaContent; scanForType = ""; }
+		if(name == "description") { metaData.content = metaContent.substring(0, 256); scanForType = ""; }
 		if(name == "determiner") { metaData.determiner = metaContent; scanForType = ""; }
 		if(name == "locale") { metaData.locale = metaContent; scanForType = ""; }
 		if(name == "site_name") { metaData.site_name = metaContent; scanForType = ""; }
@@ -133,34 +144,127 @@ function parseMetaTags(metaData) {
 	
 	// Set Image Size Correctly
 	if(metaData.image) {
-		console.log("update image size");
 		metaData.getImageSize();
 	}
 	
 	return false;
 }
 
-// Run Meta Tag Parser
-var metaData = new ogMetaData();
-parseMetaTags(metaData);
-
-
-const githubResponse = async () => {
-    const response = await fetch(DruidisHost + "/post", {
-        method: "GET",
+const submitContent = async () => {
+	
+	// Run Meta Tag Parser
+	var metaData = new ogMetaData();
+	zqqParseMetaTags(metaData);
+	console.log(metaData);
+	
+	// Gather Config Data for this site, if possible.
+	loadSiteConfig(config);
+	
+	// Submit Post
+    const response = await fetch(host + "/post", {
+        method: "POST",
 		mode: 'no-cors', // no-cors, *cors, same-origin
 		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
 		credentials: 'omit', // include, *same-origin, omit
 		headers: {
-            "Content-Type": "plain/text",
-            // "Content-Type": "application/json",
+            // "Content-Type": "plain/text",
+            "Content-Type": "application/json",
         },
-		// body: JSON.stringify(data) // body data type must match "Content-Type" header
+		body: JSON.stringify(metaData) // body data type must match "Content-Type" header
     });
     
-	// return response.json(); // For JSON Response
-    var html = await response.text(); // For HTML or Text Response
+	var html = await response.json(); // For JSON Response
+    // var html = await response.text(); // For HTML or Text Response
 	console.log(html);
 }
 
-// githubResponse();
+// submitContent();
+
+function zqqSubmitPost() {
+	const category = document.getElementById("zqqCategory");
+	confirm("Submit data?");
+	console.log("Category", category.value);
+}
+
+function createElement(element, attribute, inner) {
+	if(typeof(element) === "undefined") { return false; }
+	if(typeof(inner) === "undefined") { inner = ""; }
+	
+	const el = document.createElement(element);
+	if(typeof(attribute) === 'object') {
+		for(const attKey in attribute) {
+			el.setAttribute(attKey, attribute[attKey]);
+		}
+	}
+	
+	if(!Array.isArray(inner)) { inner = [inner]; }
+	
+	for(let k = 0; k < inner.length; k++) {
+		if(!inner[k]) { continue; }
+		if(inner[k].tagName) {
+			el.appendChild(inner[k]);
+		} else {
+			el.appendChild(document.createTextNode(inner[k]));
+		}
+	}
+	
+	return el;
+}
+
+function zqqDisplayModal() {
+	
+	// Run Meta Tag Parser
+	var metaData = new ogMetaData();
+	zqqParseMetaTags(metaData);
+	console.log(metaData);
+	
+	// Prepare HTML
+	let configOpts = "";
+	
+	if(zqqConfig.categories) {
+		for(let i = 0; i < zqqConfig.categories.length; i++) {
+			const defSelect = zqqConfig.defaultCategory === zqqConfig.categories[i] ? ` selected="selected"` : "";
+			configOpts += `<option value="${zqqConfig.categories[i]}"${defSelect}>${zqqConfig.categories[i]}</option>`;
+		}
+	}
+	
+	const comments = zqqConfig.allowComments ? `
+	<div style="font-weight:bold;font-familiy:Arial;font-size:1.2rem;padding-top:25px;">Comment</div>
+	<div>
+		<textarea id="zqqComment" name="zqqComment" style="overflow:hidden;maxlength:250;border:solid 1px #bbbbbb;width:100%;min-height:120px;"></textarea>
+	</div>` : "";
+	
+	// Prepare Modal
+	const modalContent = createElement("div", {
+		"style": "position:absolute; z-index: 9999999; left:50%;top:50%;transform:translate(-50%,-50%);width:300px;min-height:400px;padding:16px;background-color:white;border-radius:10px;border:solid 1px #dddddd;",
+	}, []);		// feedTop, feedComment, feedHov, feedSocial
+	
+	modalContent.innerHTML = `
+	<div style="font-weight:bold;font-familiy:Arial;font-size:1.2rem;text-align:center;">${metaData.title}</div>
+	
+	<div style="font-weight:bold;font-familiy:Arial;font-size:1.2rem;padding-top:25px;">Forum</div>
+	<div><input type="text" id="zqqForum" name="zqqForum" value="${zqqConfig.forum}" readonly="readonly" style="border:solid 1px #bbbbbb;width:100%;" /></div>
+	
+	<div style="font-weight:bold;font-familiy:Arial;font-size:1.2rem;padding-top:25px;">Category</div>
+	<div>
+		<select id="zqqCategory" name="zqqCategory" style="border:solid 1px #bbbbbb;width:100%;">
+			<option value="">-- No Category --</option>
+			${configOpts}
+		</select>
+	</div>
+	${comments}
+	<div style="padding-top:25px;">
+		<button onclick="zqqSubmitPost" style="width:100%;min-height:50px;background-color:#eeeeee;border:solid 1px #bbbbbb;">Submit</button>
+	</div>
+	`;
+	
+	const modal = createElement("div", {
+		"style": "position:fixed; z-index: 999999; left:0;top:0;width:100%;height:100%;background-color: rgba(0, 0, 0, 0.5);",
+	}, [modalContent]);
+	
+	// Attach Created Elements to Feed Section
+	const body = document.body;
+	body.append(modal);
+}
+
+zqqDisplayModal();
