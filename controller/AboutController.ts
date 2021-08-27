4e7ecba65de1113e89f624022800e633c13db5e4
Scionax <ScionaxNG@gmail.com>
2021-08-27 11:51:57 -0500

    About Us + FAQ Update

--- controller/AboutController.ts
+++ controller/AboutController.ts
@@ -21,15 +21,6 @@ export default class AboutController extends WebController {
 		// Cache Full Page
 		AboutController.html = `
 		${WebController.header}
-		
-		<!-- The core Firebase JS SDK is always required and must be listed first -->
-		<script src="https://www.gstatic.com/firebasejs/8.9.1/firebase-app.js"></script>
-		<script src="https://www.gstatic.com/firebasejs/8.9.1/firebase-auth.js"></script>
-		
-		<!-- Firebase UI -->
-		<script src="https://www.gstatic.com/firebasejs/ui/4.8.1/firebase-ui-auth.js"></script>
-		<link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/4.8.1/firebase-ui-auth.css" />
-		
 		${WebController.headerCloser}
 		${WebController.panelOpen}
 		${WebController.panelClose}
