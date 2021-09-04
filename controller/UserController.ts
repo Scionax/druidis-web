a92eabd6886185c3456413952c70d0e49d3f51f0
Scionax <ScionaxNG@gmail.com>
2021-09-04 16:10:58 -0500

    Logout Page, Reformatting

--- controller/UserController.ts
+++ controller/UserController.ts
@@ -5,12 +5,14 @@ export default class UserController extends WebController {
 	
 	// Cached HTML
 	static loginPage = "";
+	static logoutPage = "";
 	static signupPage = "";
 	
 	async runHandler(conn: Conn): Promise<Response> {
 		
 		// if(!conn.url2) { return await conn.sendHTML(UserController.userPage); }
 		if(conn.url2 === "login") { return await conn.sendHTML(UserController.loginPage); }
+		if(conn.url2 === "logout") { return await conn.sendHTML(UserController.logoutPage); }
 		if(conn.url2 === "sign-up") { return await conn.sendHTML(UserController.signupPage); }
 		
 		return await conn.send404();
@@ -19,6 +21,7 @@ export default class UserController extends WebController {
 	static async initialize() {
 		const decoder = new TextDecoder("utf-8");
 		UserController.loginPage = decoder.decode(await Deno.readFile(`${Deno.cwd()}/public/pages/user/login.html`));
+		UserController.logoutPage = decoder.decode(await Deno.readFile(`${Deno.cwd()}/public/pages/user/logout.html`));
 		UserController.signupPage = decoder.decode(await Deno.readFile(`${Deno.cwd()}/public/pages/user/sign-up.html`));
 	}
 }
