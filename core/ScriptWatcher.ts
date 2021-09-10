011cedb174f41f7af2890b9eadb77dcec2dbc62f
Scionax <ScionaxNG@gmail.com>
2021-09-10 18:26:29 -0500

    Watcher Architecture Upgrade

--- core/ScriptWatcher.ts
+++ core/ScriptWatcher.ts
@@ -3,12 +3,6 @@ import { minify } from "https://deno.land/x/minifier@v1.1.1/mod.ts";
 import { log } from "../deps.ts";
 import Data from "./Data.ts";
 
-// const scriptBuilder = {
-// 	druidis: {
-// 		files: [
-// 	}
-// };
-
 export default abstract class ScriptWatcher {
     
     static runUpdateTime = 0;
@@ -54,52 +48,93 @@ export default abstract class ScriptWatcher {
 	static async runScriptBuilder() {
 		log.debug("Updating and Bundling Script Files...");
 		ScriptWatcher.runUpdateTime = 0;
+		await ScriptWatcher.bundleMainScript();
+		await ScriptWatcher.bundleControllers();
+	}
+	
+	// Bundle the main files into a single script.
+	static async bundleMainScript() {
 		
 		// Prepare Important Directories & Files
 		const scriptDir = "../../scripts";
-		const buildFile = `${scriptDir}/_build.ts`;
-		const thisProjectFile = `./public/druidis.js`;
-		const scriptProjectFile = `${scriptDir}/_build.js`;
+		const buildFile = `../../build/druidis.ts`;
+		const saveTo = `./public/druidis.js`;
 		
 		// Get a list of the scripts. Ignore any preceded with "_" or with .js
-		// const fileNames: string[] = [];
-		let typescriptContent = "";
-		
+		let tsContent = "";
 		
 		for await(const dirEntry of await Data.getFilesRecursive(scriptDir)) {
 			
-			// Get all .ts files. Ignore any files that are preceded with "_" or with .js
-			if(dirEntry.indexOf(".ts") > -1 && dirEntry.indexOf("/_") !== 0 && dirEntry.indexOf("/tests") == -1) {
-				let fileContents = await Deno.readTextFile(`${scriptDir}${dirEntry}`);
-				console.log(dirEntry);
+			// Get all .ts files. Ignore any files with "/tests", "/controllers", or preceded with "_"
+			if(dirEntry.indexOf(".ts") > -1 && dirEntry.indexOf("/_") !== 0) {
+				const fileContents = await Deno.readTextFile(`${scriptDir}${dirEntry}`);
 				
-				typescriptContent += `
-				${fileContents.replaceAll(/^import.*/gm, "")}`;
+				tsContent += `
+				${fileContents}`;
 			}
 		}
 		
-		// Remove disallowed content:
-		typescriptContent = typescriptContent.replaceAll(/^import.*/gm, "");				// Remove all import lines
-		typescriptContent = typescriptContent.replaceAll("export default", "");				// Remove 'export default' values
-		typescriptContent = typescriptContent.replaceAll("export function", "function");	// Remove 'export function' values
+		tsContent = ScriptWatcher.removeBreakingTSTags(tsContent);
 		
-		// console.log(typescriptContent);
 		// Combine the Scripts into a single file named "_build.ts"
-		await Deno.writeTextFile(buildFile, typescriptContent);
+		await Deno.writeTextFile(buildFile, tsContent);
 		
 		// Run the Deno Bundler on the script.
 		const { files } = await Deno.emit(buildFile, { bundle: "module", compilerOptions: { allowUnreachableCode: true }  });
-		console.log(files);
 		let bundledJs = files["deno:///bundle.js"];
 		
 		// Minifiy the JS Code
 		bundledJs = minify("js", bundledJs);
 		
 		// Copy the Bundled JS. Save to a new file in both projects.
-		await Deno.writeTextFile(thisProjectFile, bundledJs);
-		await Deno.writeTextFile(scriptProjectFile, bundledJs);
+		await Deno.writeTextFile(saveTo, bundledJs);
 		
-		log.debug(`Updated druidis.js script.`);
+		log.debug(`Updated ${saveTo} script.`);
 	}
+	
+	// Bundle the controllers, each into their own script.
+	static async bundleControllers() {
+		
+		// Prepare Important Directories & Files
+		const scriptDir = "../../scripts/controllers";
+		const buildDir = `../../build`;
+		const saveTo = `./public/scripts`;
+		
+		// Get a list of the scripts. Ignore any preceded with "_" or with .js
+		for await(const dirEntry of await Data.getFilesRecursive(scriptDir)) {
+			
+			// Only retrieve .ts files within the /controllers directory:
+			if(dirEntry.indexOf(".ts") === -1) { return; }
+			
+			const controller = dirEntry.replace("/", "").replace(".ts", "");
+			
+			let fileContents = await Deno.readTextFile(`${scriptDir}${dirEntry}`);
+			fileContents = ScriptWatcher.removeBreakingTSTags(fileContents);
+			
+			// Combine the Scripts into a single file named "/build/{controller}.ts"
+			await Deno.writeTextFile(`${buildDir}/${controller}.ts`, fileContents);
+			
+			// Run the Deno Bundler on the script.
+			const { files } = await Deno.emit(`${buildDir}/${controller}.ts`, { bundle: "module", compilerOptions: { allowUnreachableCode: true }  });
+			let bundledJs = files["deno:///bundle.js"];
+			
+			// Minifiy the JS Code
+			bundledJs = minify("js", bundledJs);
+			
+			// Copy the Bundled JS. Save to a new file in both projects.
+			await Deno.writeTextFile(`${saveTo}/${controller}.js`, bundledJs);
+		
+			log.debug(`Updated ${saveTo}/${controller}.js`);
+		}
+	}
+	
+	// Remove content from a .ts file that would break the bundling process to javascript (.js file)
+	static removeBreakingTSTags(tsContent: string) {
+		tsContent = tsContent.replaceAll(/^import.*/gm, "");				// Remove all import lines
+		tsContent = tsContent.replaceAll("export default", "");				// Remove 'export default' values
+		tsContent = tsContent.replaceAll("export function", "function");	// Remove 'export function' values
+		return tsContent;
+	}
+	
 }
 
